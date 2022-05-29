a0faec8d4cfda7458452506f09101525427c8b1a
nekohasy <63384336+nekohasy@users.noreply.github.com>
2022-05-29 16:59:58 +0900

    AddDetailsToReservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_29_040702) do
+ActiveRecord::Schema.define(version: 2022_05_29_075746) do
 
   create_table "reservations", force: :cascade do |t|
     t.date "start_date"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2022_05_29_040702) do
     t.integer "person_num"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "room_id"
+    t.integer "user_id"
+    t.integer "total_amount"
   end
 
   create_table "rooms", force: :cascade do |t|
