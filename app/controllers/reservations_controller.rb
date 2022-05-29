6cfda4ec84279909c7fd835af319680aca6641aa
nekohasy <63384336+nekohasy@users.noreply.github.com>
2022-05-29 16:52:57 +0900

    Create index.html & new.html

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,12 +3,22 @@ class ReservationsController < ApplicationController
   end
 
   def new
+    @reservation = Reservation.new
+    @room = Room.find(params[:room_id])
+    @room_id = params[:room_id]
+    @user_id = params[:user_id]
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+    @person_num = params[:person_num]
+    @reservation.total_day = @end_date.to_date - @start_date.to_date
+    @total_amount = @room.price * @person_num.to_i * @reservation.total_day
   end
 
   def create
   end
 
   def show
+    @reservation = Reservation.find(params[:id])
   end
 
   def edit
