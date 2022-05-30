f21b33148439cc76f8245e7f48b47d82f17abe2d
nekohasy <63384336+nekohasy@users.noreply.github.com>
2022-05-30 16:32:43 +0900

    Create Account Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root "top#index"
   devise_for :users
+
+  resources :users do
+    collection do
+      get 'account'
+    end
+  end
+
   resources :rooms do
     collection do
       get 'posts'
