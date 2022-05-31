7d5c7bf531944424903a11c1b01d16365f3b455e
nekohasy <63384336+nekohasy@users.noreply.github.com>
2022-05-31 14:09:36 +0900

    AddUser_idToRooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_030453) do
+ActiveRecord::Schema.define(version: 2022_05_31_050841) do
 
   create_table "reservations", force: :cascade do |t|
     t.date "start_date"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2022_05_30_030453) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
